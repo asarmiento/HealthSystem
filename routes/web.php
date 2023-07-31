ec89024123d439ce70c2c066e10f3e6e048a42ef
Jackson <kaxsonfernand14@gmail.com>
2023-07-31 11:43:44 -0600

    feat: creacion de rutas ..

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,15 @@ Route::get("hospital/{id}/edit",[HospitalsController::class,'index']);
 Route::get("hospital-delete/{id}",[HospitalsController::class,'delete']);
 Route::post("hospital-store",[HospitalsController::class,'store']);
 Route::put("hospital-update/{id}",[HospitalsController::class,'update']);
+
+Route::get("data-users",[UsersController::class,'index']);
+Route::get("users/{id}/edit",[UserController::class,'index']);
+Route::get("users-delete/{id}",[UsersController::class,'delete']);
+Route::post("users-store",[UsersController::class,'store']);
+Route::put("users-update/{id}",[UsersController::class,'update']);
+
+Route::get("data-vehicles",[VehiclesController::class,'index']);
+Route::get("vehicles/{id}/edit",[VehiclesController::class,'index']);
+Route::get("vehicles-delete/{id}",[VehiclesController::class,'delete']);
+Route::post("vehicles-store",[VehiclesController::class,'store']);
+Route::put("vehicles-update/{id}",[VehiclesController::class,'update']);
