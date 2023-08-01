0c1716bf7b86683f3ad6df49f59a78fcd061092f
Derekpro4488 <141046881+Derekpro4488@users.noreply.github.com>
2023-08-01 10:14:11 -0600

    fix
    correccion de modulo de control

--- app/Http/Controllers/PantientsController.php
+++ app/Http/Controllers/PantientsController.php
@@ -45,7 +45,7 @@ class PantientsController extends Controller
             $driver = new Patient();
             $driver->last_name = $data['last_name'];
             $driver->cell = $data['cell'];
-            $driver->cell = $data['name'];
+            $driver->name = $data['name'];
             $driver->phone = $data['phone'];
             $driver->email = $data['email'];
             $driver->hospital_id = $data['hospital_id'];
